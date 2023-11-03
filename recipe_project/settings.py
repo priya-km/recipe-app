7758e55156107f5da0b0b160a365248bf3237fff
priya-km <priya.k.mah@gmail.com>
2023-11-03 16:50:47 -0400

    Fixing issues

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -25,17 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpevh4xz)3w)6xuq6kv7g&3^xf^)gr-n3&p#%9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
-if not IS_HEROKU:
-    DEBUG = True
-
-if IS_HEROKU:
-    DEBUG = True
-
-if IS_HEROKU:
-    ALLOWED_HOSTS = ['*']
-else:
-    ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://pkm-recipes-775734c521de.herokuapp.com']
 
 
 # Application definition
